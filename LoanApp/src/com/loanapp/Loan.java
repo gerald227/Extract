c0a7b14449d95cf60a90c7c8e9b2f949aa80d23f
Gerald <g_dcunha@yahoo.com>
2020-08-05 18:42:40 -0400

    First Commit

---
+++ LoanApp/src/com/loanapp/Loan.java
@@ -0,0 +1,36 @@
+package com.loanapp;
+
+
+public class Loan {
+	private String username;
+	private double amount;
+	private String state;
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public boolean isapproved(){
+	    return true;
+	}
+}
